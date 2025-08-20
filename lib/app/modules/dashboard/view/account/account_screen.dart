89932ebe3ac99ddfab740903ef4c46bd9048faec
NeesargD <nisd997@gmail.com>
2025-08-20 14:43:43 -0700

    handling logout on account screen

--- lib/app/modules/dashboard/view/account/account_screen.dart
+++ lib/app/modules/dashboard/view/account/account_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:timoraa/app/core/widgets/buttons/app_elevated_button.dart';
+import 'package:timoraa/app/utils/constants/route_name.dart';
+import 'package:timoraa/app/utils/extensions/navigation_extension.dart';
 import 'package:timoraa/app/utils/services/app_state.dart';
 
 import '../../../../utils/constants/color_constants.dart';
@@ -16,33 +18,36 @@ class AccountPage extends StatelessWidget {
       children: [
         Center(child: Text("Manage Your Profile")),
         const Gap(20),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: AppElevatedButton(
-            Text(
-              "Logout",
-              style: TextStyle(
-                color: ColorConstants.whiteColor,
-                fontSize: 14,
-                fontWeight: FontWeight.w600,
-                fontFamily: "PlusJakartaSans",
+        if (appState.userId.isNotEmpty)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: AppElevatedButton(
+              Text(
+                "Logout",
+                style: TextStyle(
+                  color: ColorConstants.whiteColor,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: "PlusJakartaSans",
+                ),
               ),
+              onPressed: () {
+                appState.clearAllValues();
+                appState.cartItems.clear();
+                appState.setUserImage = '';
+                appState.setUserId = "";
+                appState.setSessionId = '';
+                appState.setUserName = '';
+                appState.selectedTimeSlot.value = '';
+                appState.selectedSlotInfo.value = '';
+                appState.selectedSaloon.value = '';
+                appState.selectedSaloonAddress.value = '';
+                appState.totalPrice.value = 0.0;
+                appState.appPageIndex.value = 0;
+                context.pushReplacementNamed(RouteName.splashScreen);
+              },
             ),
-            onPressed: () {
-              appState.clearAllValues();
-              appState.cartItems.clear();
-              appState.setUserImage = '';
-              appState.setUserId = "";
-              appState.setSessionId = '';
-              appState.setUserName = '';
-              appState.selectedTimeSlot.value = '';
-              appState.selectedSlotInfo.value = '';
-              appState.selectedSaloon.value = '';
-              appState.selectedSaloonAddress.value = '';
-              appState.totalPrice.value = 0.0;
-            },
           ),
-        ),
       ],
     );
   }
